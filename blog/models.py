8fcada641de4db38a0469c637ac08d061d9559e1
Alex Zmachynski <bird5555@Alexs-Mac-mini.local>
2025-06-02 19:44:06 -0700

    Blog and all

--- blog/models.py
+++ blog/models.py
@@ -6,9 +6,11 @@ class Blog(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
 
-    
-    # def summary(self):
-    #     return self.body[:100]
+    def __str__(self):
+        return self.title
 
-    # def pub_date_pretty(self):
-    #     return self.pub_date.strftime('%b %e %Y') 
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y') 
